d891fbc935a32eeea4187cf8cdf5438bb3011f23
Ebuka <ebuka2264@yahoo.com>
2023-08-19 23:11:16 +0100

    rm-vrf

--- core/src/Stella.sol
+++ core/src/Stella.sol
@@ -10,18 +10,12 @@ import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
  * @notice The token is not live
  * @dev This contract implements chailink vrf
  */
- contract Stella is ERC20, VRFConsumerBaseV2{
+ contract Stella is ERC20{
 
     error InsufficientFunds();
     
-    bytes32 private immutable i_gasLane;
-    uint64  private immutable subscriptionID;
-    uint32 private immutable i_callbackGasLimit;
-    uint8 private constant request_confirmations = 3;
-    uint8 private constant num_words = 1;
     uint256 private immutable pool; 
 
-    VRFCoordinatorV2Interface private immutable i_coordinator;
 
     struct Status{
         bool exists;
@@ -38,54 +32,15 @@ import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
     event exactTokenSwapped(address indexed t0, uint indexed tokenAmount, uint indexed EthAmount);
 
     constructor(
-        uint256 initialSupply, 
-        uint64 _subscriptionID, 
-        address vrfcoordinator,
-        bytes32 gasLane,
-        uint32 callbackGasLimit
+        uint256 initialSupply
         ) 
     ERC20("STELLA","STS")
-    VRFConsumerBaseV2(vrfcoordinator) payable {
-        subscriptionID = _subscriptionID;
-        i_coordinator = VRFCoordinatorV2Interface(vrfcoordinator);
-        i_gasLane = gasLane;
-        i_callbackGasLimit = callbackGasLimit;
+     payable {
         pool = initialSupply * msg.value;
         _mint(address(this),initialSupply);
      }
     
-    function airdrop(address to) public virtual {
-        address owner = address(this);
-        uint amount = 10000000000;
-        require(balanceOf(owner) > amount, "Reserve is dry");
-        require(balanceOf(to) <= 0, "Received ICO before");
-        _transfer(owner,to,amount);
-        emit Airdrop(to,amount);    
-    }
-
-    function lottery()public virtual {
-        require(balanceOf(msg.sender) <= 0, "Received ICO before");
-        uint256 requestID = requestRandomWords();
-        address owner = address(this);
-        require(randomness[requestID].exists, 'Please try again Later');
-        uint amount = 1000000000 * randomness[requestID].words[0];
-        _transfer(owner,msg.sender,amount);
-        emit Lottery(msg.sender, amount);
-    }
-
-    function fulfillRandomWords(uint256 _requestId, uint256[] memory _randomWords) internal virtual override{
-        randomness[_requestId] = Status(true,false,_randomWords);   
-    }
-
-    function requestRandomWords()internal returns(uint requestId){
-        requestId = i_coordinator.requestRandomWords(
-            i_gasLane,
-            subscriptionID,
-            request_confirmations,
-            i_callbackGasLimit,
-            num_words
-        );
-    }
+    
     /**
     implements a token swap, we assume ETH is an ERC20 TOKEN
     x*y = K 
