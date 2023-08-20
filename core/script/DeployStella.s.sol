1cb6591ea95557c0fdd0390b3575aee04d608f9e
Ebuka <ebuka2264@yahoo.com>
2023-08-20 02:06:39 +0100

    clean

---
+++ core/script/DeployStella.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+import { Stella } from "../src/Stella.sol";
+import { Script, console2 } from "forge-std/Script.sol";
+
+contract DeployStella is Script {
+    constructor(){}
+
+    function run() external returns (Stella sts) {
+        uint256 initialSupply = 5000e18;
+        uint256 amount = 7000 ether;
+        
+        if (block.chainid == 31337){            
+            vm.startBroadcast();
+            sts = new Stella{value:amount}(initialSupply);
+            vm.stopBroadcast();
+        }
+    }
+}
